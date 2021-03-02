528fcf4161226aa64206824949a85e0c5c47fb3b
asiyebulut <atubakesenel@gmail.com>
2021-03-02 12:56:18 -0800

    ready to tell your fortune

---
+++ main.js
@@ -0,0 +1,9 @@
+var numOfChildren = 1;
+console.log(numOfChildren);
+var partnersName = 'Ahmet'
+console.log(partnersName);
+var location = 'Seattle';
+console.log(location);
+var jobTitle = 'Web Developer';
+console.log(jobTitle);
+console.log('You will be a ' + jobTitle + ' in ' + location + ', ' + 'and married to ' + partnersName + ' with ' + numOfChildren + ' kids.');
